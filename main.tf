22e18887d42710e49ffc37272492d2a80452344f
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-03-29 20:36:19 +0900

    add:参照

--- main.tf
+++ main.tf
@@ -1,21 +1,33 @@
-data "aws_ami" "recent_amazon_linux_2" {
-  most_recent = true
-  owners = ["amazon"]
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
 
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-2.0.??????????-x86_64-gp2"]
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  filter {
-    name = "state"
-    values = ["available"]
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "example" {
-  ami = data.aws_ami.recent_amazon_linux_2.image_id
+  ami = "ami-0c3fd0f5d33134a76"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
 }
 
-
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
+}
